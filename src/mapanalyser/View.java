eb615871e42ab02867c5febde947a3ba748613c0
Jaiprakash siyyadri <jaiprakash.siyyadri@gramener.org>
2019-05-20 14:40:39 +0530

    WIP: console interaction has been added

--- src/mapanalyser/View.java
+++ src/mapanalyser/View.java
@@ -5,6 +5,7 @@
  */
 package mapanalyser;
 import mapanalyser.MAPAnalyser;
+import java.util.Scanner;
 
 /**
  *
@@ -13,9 +14,49 @@ import mapanalyser.MAPAnalyser;
 public class View {
     private MAPAnalyser a;
     public View(MAPAnalyser a){
-        
+        this.a = a;
     }
     public void commandLoop(){
-        
+        boolean continue_loop = true;
+        Scanner reader = new Scanner(System.in);
+
+        while(continue_loop){
+            System.out.println("The following commands are recognized");
+            System.out.println("Display this message                                    > 0");
+            System.out.println("Display a specific subject record                       > 1 id");
+            System.out.println("Display records for all subject recordswithin a range   > 2 map1 map2");
+            System.out.println("Display statistics (minimum, maximum and median)        > 3");
+            System.out.println("Exit the application                                    > 9");
+            
+            int option = reader.nextInt();
+
+            switch(option){
+                case 0:
+                        System.out.println("Display the same message again");
+                        break;
+                case 1: 
+                        // find the recod eith this id
+                        String id = reader.next();
+                        System.out.println("The id "+id);
+                        break;
+                case 2: 
+                        // subjects with in range map1 map2
+                        int map1 = reader.nextInt();
+                        int map2 = reader.nextInt();
+                        break;
+                case 3: 
+                        // display statistics
+                        System.out.println("Lowest MAP");
+                        System.out.println("Highest MAP");
+                        System.out.println("Median MAP");
+                        break;
+                case 9: 
+                        continue_loop = false;
+                        System.out.println("Thanks..");
+                        break;
+            }
+
+        }
+        reader.close();
     }
 }
